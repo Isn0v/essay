4c69ec3e24043d7d9521b7f596c10b9a25e04133
Sergey <isnov.contact@gmail.com>
2025-03-28 13:15:18 +0700

    refactor the initial structure

---
+++ build.sh
@@ -0,0 +1,18 @@
+TARGET=target
+TEXFILE=paper
+BIB=my.bib
+
+rm -r $TARGET
+mkdir $TARGET
+cd $TARGET
+
+cp -r ../src/* .
+
+pdflatex $TEXFILE.tex
+bibtex $TEXFILE
+pdflatex $TEXFILE.tex
+pdflatex $TEXFILE.tex
+
+mv paper.pdf ..
+cd ..
+open paper.pdf
