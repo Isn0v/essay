856d7457f21b414573c3f92637acf525eb437bce
Sergey <isnov.contact@gmail.com>
2025-04-01 21:29:09 +0700

    Essay 2.0

---
+++ build.sh
@@ -0,0 +1,16 @@
+TARGET=target
+TEXFILE=paper
+BIB=my.bib
+
+rm -r $TARGET
+mkdir $TARGET
+cd $TARGET
+
+cp -r ../src/* .
+
+pdflatex $TEXFILE.tex
+bibtex $TEXFILE
+pdflatex $TEXFILE.tex
+pdflatex $TEXFILE.tex
+
+mv paper.pdf ..
